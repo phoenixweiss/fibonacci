3b7a9b24cd6051784e08601356be32a2a451dc69
PAVEL TKACHEV <phoenixweiss@ya.ru>
2022-02-25 01:53:29 +0300

    benchmark

---
+++ modules/benchmark.js
@@ -0,0 +1,20 @@
+// Измеряет время выполнения функции в мкс
+
+function benchmark(fn) {
+
+    let b = {}
+
+    let start = process.hrtime();
+
+    fn
+
+    let stop = process.hrtime(start);
+
+    b.output = fn
+    b.hrtime = (stop[0] * 1e9 + stop[1]) / 1e9
+
+    return b
+
+}
+
+export { benchmark as default };
