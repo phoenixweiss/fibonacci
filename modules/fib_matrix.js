4595bcfd537a59d9774168e6f9c984d1d573ead8
PAVEL TKACHEV <phoenixweiss@ya.ru>
2024-01-04 00:06:44 +0300

    finally closed the gestalt

--- modules/fib_matrix.js
+++ modules/fib_matrix.js
@@ -1,9 +1,42 @@
 // Матричный алгоритм
 
-// TODO: Доделать!
-
 function fib(n) {
-  return 'Not ready yet';
+  // Создаем Q-матрицу
+  const qMatrix = [[1, 1], [1, 0]];
+
+  // Функция для умножения матриц
+  function multiplyMatrix(a, b) {
+    const result = [];
+    for (let i = 0; i < a.length; i++) {
+      result[i] = [];
+      for (let j = 0; j < b[0].length; j++) {
+        let sum = 0;
+        for (let k = 0; k < a[0].length; k++) {
+          sum += a[i][k] * b[k][j];
+        }
+        result[i][j] = sum;
+      }
+    }
+    return result;
+  }
+
+  // Функция для возведения матрицы в степень
+  function powerMatrix(matrix, power) {
+    let result = [[1, 0], [0, 1]];
+    while (power > 0) {
+      if (power % 2 === 1) {
+        result = multiplyMatrix(result, matrix);
+      }
+      matrix = multiplyMatrix(matrix, matrix);
+      power = Math.floor(power / 2);
+    }
+    return result;
+  }
+
+  // Умножаем Q-матрицу на [1, 0] для получения n-го числа Фибоначчи
+  const resultMatrix = multiplyMatrix(powerMatrix(qMatrix, n - 1), [[1], [0]]);
+
+  return resultMatrix[0][0];
 }
 
 export { fib as default };
